2ccc46050639f39013a6aad47eddcb0f5a226262
Silvio Cavalcanti Bonfim <silviocavalcanti2011@gmail.com>
2023-09-15 13:45:02 -0300

    1509

---
+++ Programação em Novas Tecnologias Desktop (JAVA)/Atividade 1/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+    public static void main(String[] args) {
+        String nome = "Silvio";
+        String sobreNome = "Cavalcanti";
+        String estadoCivil = "Solteiro";
+        int idade = 26;
+
+        System.out.println("Nome: "+nome);
+        System.out.println("Sobre Nome: "+sobreNome);
+        System.out.println("Idade: "+idade);
+        System.out.println("Estado Civil: "+estadoCivil);
+    }
+}
